7bcc2df058755c9cee4c3f811bf9985a44037c86
Bruno Carballido <bcarballido@ceibal.edu.uy>
2022-08-22 09:47:08 -0300

    UPDATE, se actualizan archivos para inicio de proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+const personas = [
+  {
+    "nombre": "Juan",
+    "edad": 39
+  },
+  {
+    "nombre": "Laura",
+    "edad": 89
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>El amor es lo mas</p>
+      {
+        personas.map(persona => (
+          <div className="box">
+            <p>Nombre: {persona.nombre}</p>
+            <p>Edad: {persona.edad}</p>
+            <hr />
+          </div>
+        ))
+      }
     </div>
   );
 }
