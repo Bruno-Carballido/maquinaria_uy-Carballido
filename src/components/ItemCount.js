bc9c96ce43c40da847782c3e02bbcce10602ab2b
Bruno Carballido <bcarballido@ceibal.edu.uy>
2022-09-07 08:54:40 -0300

    FIX, se agrega Alert faltante

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,14 +4,19 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import React, { useState } from 'react';
 
 const ItemCount = ({ stock, initial, onAdd }) => {
-    const [valor, setValor] = useState(0)
+    const [valor, setValor] = useState(Number(initial))
     return (
-        <div className="col-1">
-            <InputGroup className="mb-3">
-                <Button onClick={() => { if (valor > 0) { setValor(valor - 1) } }} variant="outline-secondary">-</Button>
-                <Form.Control value={valor} readOnly />
-                <Button onClick={() => { if (valor < stock) { setValor(valor + 1) } }} variant="outline-secondary">+</Button>
-            </InputGroup>
+        <div className="col-2 card">
+            <div className="card-body">
+                <InputGroup className="mb-3">
+                    <Button onClick={() => { if (valor > 0) { setValor(valor - 1) } }} variant="outline-secondary">-</Button>
+                    <Form.Control value={valor} readOnly />
+                    <Button onClick={() => { if (valor < stock) { setValor(valor + 1) } }} variant="outline-secondary">+</Button>
+                </InputGroup>
+                <div className="d-flex justify-content-center">
+                    <Button onClick={() => onAdd(stock, valor)} variant="outline-primary">Agregar al carrito</Button>
+                </div>
+            </div>
         </div >
     );
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,7 +4,7 @@ const ItemListContainer = ({ mensaje }) => {
     return (
         <div>
             <p>Bienvenidos a la web</p>
-            <ItemCount stock='5' initial='0' />
+            <ItemCount stock='5' initial='0' onAdd={(stock_disp, cantidad) => { if (stock_disp && cantidad > 0) { alert('Se han agregado ' + cantidad + ' de items al carrito.') } }} />
         </div>
     );
 }
